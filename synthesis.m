1af14bc4bfd50e18c067790b83c1e07b88c96232
naomichoy <41099656+naomichoy@users.noreply.github.com>
2023-11-05 18:07:13 +0000

    synthesized audio plot

--- synthesis.m
+++ synthesis.m
@@ -2,7 +2,7 @@ clear all;
 
 % load lpc coefficient
 path='C:\Users\cat97\Documents\UniofSurrey\sem1\EEEM030-speech\assignment1\';
-fname = 'heed_m_lpc10_2400.mat';
+fname = 'heed_m_lpc30_2400.mat';
 fullpath=strcat(path,fname)
 load(fullpath,'lpc_coefficients');
 
@@ -17,15 +17,39 @@ Fs = 24000;
 f1_p = Fs/f1
 
 % generate excitation signal
-pulse_train = zeros(1, num_samples*10); % Define the duration
+pulse_train = zeros(1, Fs); % Define the duration
 pulse_train(1:f1_p:end) = 1;
 
-% filter pulse train with lpc coefficients
+% add all-pole filter to pulse train with lpc coefficients
 synthesized_signal = filter(1, lpc_coefficients, pulse_train);
+% synthesized_signal = conv(pulse_train, lpc_coefficients);
 % pwr = 1.0193
 % synthesized_signal = synthesized_signal/ max(abs(synthesized_signal)) * pwr;
 
-% output
+% plot output signal
+figure(1);
+t = (0:length(synthesized_signal) - 1) / Fs;
+plot(t, synthesized_signal, 'b-');
+xlabel('Time (seconds)');
+ylabel('Amplitude');
+title('Sound Waveform');
+
+N = length(synthesized_signal);
+frequencies = (0:N-1) * Fs / N;
+amplitude_spectrum = abs(fft(synthesized_signal));
+amplitude_spectrum_db = 20 * log10(amplitude_spectrum);
+
+figure(3);
+plot(frequencies, amplitude_spectrum_db);
+xlabel('Frequency (Hz)');
+ylabel('Amplitude (dB)');
+title('Amplitude Spectrum of Synthesised Signal');
+
+
+% [H,W]=freqz(1,[1 -A],512)
+
+
+% save output
 sound(synthesized_signal, Fs);
 outfile = split(fname,".");
 outfile = strcat(outfile{1}, ".wav");
